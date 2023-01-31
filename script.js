6c0c6821acad11fce2cf308ce06140ef334af86c
Galo Vega <contacto.detodounpocomx@gmail.com>
2023-01-30 22:40:17 -0600

    solving nav problems'

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-
-
-
-const q = document.querySelectorAll(".q");
-const a = document.querySelectorAll(".a");
-const arr = document.querySelectorAll(".arrow");
+/*const q = document.querySelector(".q");
+const a = document.querySelector(".a");
+const arr = document.querySelector(".arrow");
 
 for(let i = 0; i < q.length; i++) {
     q[i].addEventListener("click", () => {
         a[i].classList.toggle("a-opened");
         arr[i].classList.toggle("arrow-rotated");     
     });
-}
+} //
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle(".nav-menu-visible")
+})
+
+//
+
+document.querySelector(".nav-toggle").addEventListener("click", () => {
+    document.querySelector(".nav-menu-visible").style.left = 0%
+}) 
+
+*/
+
+const navToggle = document.querySelector("#nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navMenuVisible = document.querySelector(".nav-menu-visible");
+ 
+
+navToggle.addEventListener("click", function onClick(event) {
+    navMenu.classList.toggle(".nav-menu-visible")
+}) 
+
