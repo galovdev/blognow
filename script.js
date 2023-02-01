07b5f6f9144028b16bfa07706f59fa0755ffc026
Galo Vega <contacto.detodounpocomx@gmail.com>
2023-01-31 22:16:22 -0600

    toogle nav bar problem solved

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const q = document.querySelector(".q");
+const q = document.querySelector(".q");
 const a = document.querySelector(".a");
 const arr = document.querySelector(".arrow");
 
@@ -7,27 +7,11 @@ for(let i = 0; i < q.length; i++) {
         a[i].classList.toggle("a-opened");
         arr[i].classList.toggle("arrow-rotated");     
     });
-} //
+} 
 
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(".nav-menu-visible")
-})
-
-//
-
-document.querySelector(".nav-toggle").addEventListener("click", () => {
-    document.querySelector(".nav-menu-visible").style.left = 0%
-}) 
-
-*/
 
 const navToggle = document.querySelector("#nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-const navMenuVisible = document.querySelector(".nav-menu-visible");
- 
+const navMenu = document.querySelector(".nav-menu"); 
 
 navToggle.addEventListener("click", function onClick(event) {
     navMenu.classList.toggle("nav-menu-visible")
